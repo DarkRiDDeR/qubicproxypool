e93f57927c5427842720d03a3e1de115ddc0a22a
DarkRiDDeR <darkridder.pro@gmail.com>
2024-04-16 15:22:33 +0800

    fix getTimestamp and add getCurrent epoch

--- console.js
+++ console.js
@@ -2,8 +2,8 @@
 import { argv } from 'node:process'
 import { stat } from 'node:fs'
 import { match } from 'node:assert'
-import { confLogger, users } from "./config.js"
-import { dbConnect, dbCreateUser } from "./functions.js"
+import { confLogger, confUsers } from "./config.js"
+import { dbConnect, dbCreateUser, getCurrentEpoch, getTimestampOfLastWednesday } from "./functions.js"
 
 /*Launching the Node.js process as:
 node process-args.js one two=three four 
@@ -42,7 +42,7 @@ if (argv[2] == 'install') {
     if (argv.length != 7) {
         console.error('Error: add user command "adduser <login> <email> <password> <wallet>"')
     } else {
-        console.log(dbCreateUser(dbc, argv[3].toLowerCase(), argv[4].toLowerCase(), argv[5], argv[6]))
+        console.log(await dbCreateUser(dbc, argv[3].toLowerCase(), argv[4].toLowerCase(), argv[5], argv[6]))
     }
 } else if (argv[2] == 'getuser' && argv[3]) {
     try {
@@ -53,8 +53,15 @@ if (argv[2] == 'install') {
     } catch (err) {
         console.log(err)
     }
+} else if (argv[2] == 'epoch') {
+    let [epoch, progress] = getCurrentEpoch()
+    progress = Math.round(progress * 10000) / 100
+    let startDate = new Date(getTimestampOfLastWednesday())
+    console.log(`Epoch=${epoch}; Progress: ${progress}%; Start date: ` + startDate.toISOString())
 } else if (argv[2] == 'calculate') {
-    
+
+} else {
+    console.error('Error: command not find')
 }
 
 try {
@@ -62,15 +69,3 @@ try {
 } catch(err) { 
     log.warning('DB end error: ' + err.message)
 }
-
-
-
-
-
-
-/*
-const resDbEnd = dbEnd(dbc)
-if (resDbEnd !== true) {
-    logger.error('DB end error: %s', resDbEnd);
-    process.exit(1)
-}*/
--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 import { createHash } from 'node:crypto'
 import mysql from 'mysql2/promise'
-import { confDb } from './config.js'
+import { confDb, confEpoch} from './config.js'
 
 export function getPasswordHash(password) {
     return createHash('sha256').update('QubicPowerProxy' + password).digest('hex')
@@ -9,10 +9,23 @@ export function getPasswordHash(password) {
 /**
  * @returns int Timestamp in milliseconds of last Wednesday. Example:  Wed, 13 Mar 2024 12:00:00 GMT = 1710331200
  */
-export function getTimestampOfLastWednesday () {
+export function getTimestampOfLastWednesday() {
     const date = new Date
-    let time = date.getTime() % 604800000
+    let time = date.getTime() % 604800000 + 43200000
     return date.getTime() - time
+}
+
+/**
+ * 
+ * @returns array [epoch, progress]
+ */
+export function getCurrentEpoch() {
+    const date = new Date()
+    let progress = (date.getTime() - confEpoch.timestamp) / 604800000
+    let epoch = Math.floor(progress)
+    progress = progress - epoch
+    epoch += confEpoch.number
+    return [epoch, progress]
 }
 
 /**
